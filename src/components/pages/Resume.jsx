e95ae941ea392f8817933b26c76b9dd1dc578adf
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-10-20 13:40:43 -0400

    Added content to resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,14 +1,39 @@
+import pdfFile from "../../assets/KClark_Resume_2023.pdf";
+import "../../styles/Resume.css";
+
 export default function Resume() {
   return (
-    <div>
+    <div className="resume-container">
       <h1 className="title-name">Resume</h1>
+      <a
+        href={pdfFile}
+        download="Katherine_Clark_Resume.pdf"
+        className="download-link"
+      >
+        Download
+      </a>
+      <h2>Technical Skills</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        <b>Esri Suite</b> ArcGIS Online, Enterprise Portal, Pro, ArcMap, Web App
+        Builder, Experience Builder, Field Applications Suite
+        <br></br>
+        <b>APIs/Extensions </b>
+        ArcPy, ArcGIS API for Python, Spatial Analyst and 3D Analyst extensions
+        <br></br>
+        <b>Programming Languages/Tools</b> Python, Javascript, CSS, HTML, Git,
+        node.js
+        <br></br>
+        <b>Productivity Tools</b> Microsoft Office Suite, GitHub, GitLab
+      </p>
+      <p className="divider"></p>
+      <h2>Professional Experience</h2>
+      <p>
+        <b>Geospatial Analyst III</b> - Resource Environmental Solutions, LLC
+        (2017 - present)
+        <br></br>
+        <b>Fish Ecology Lab Research Assistant </b>- George Mason University
+        (2015-2016)
+        <br></br>
       </p>
     </div>
   );
