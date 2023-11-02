9dac1e1fb5290fb5f1794dd18d3cfb7d033dbe54
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-11-02 17:32:55 -0400

    Correcting project thumbnail references and adding final bootcamp project (NPS Trail Viewer) to portfolio project page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,45 +1,59 @@
 import Project from "../Project";
 import "../../styles/Project.css";
+import passwordGen from "../../assets/password-gen.jpeg";
+import quizGame from "../../assets/quiz-question.jpeg";
+import puppyLove from "../../assets/puppy-love-husky.jpg";
+import pomodoro from "../../assets/pomodoro.jfif";
+import weatherUnderground from "../../assets/wu.png";
+import planner from "../../assets/planner.jpeg";
+import npsHikes from "../../assets/nps.jpg";
 
 export default function Projects() {
   const projectsData = [
     {
+      repo: "https://github.com/KClarkDev/nps-trail-viewer",
+      url: "https://nps-trail-viewer-493315c88a96.herokuapp.com/",
+      imgUrl: npsHikes,
+      title: "NPS Trail Viewer",
+      details: "ArcGIS API for Javascript",
+    },
+    {
       repo: "https://github.com/KClarkDev/password-generator",
       url: "https://kclarkdev.github.io/password-generator/",
-      imgUrl: "./src/assets/password-gen.jpeg",
+      imgUrl: passwordGen,
       title: "Password Generator",
       details: "Javascript",
     },
     {
       repo: "https://github.com/KClarkDev/timed-quiz-game",
       url: "https://kclarkdev.github.io/timed-quiz-game/",
-      imgUrl: "./src/assets/quiz-question.jpeg",
+      imgUrl: quizGame,
       title: "Timed Quiz Game",
       details: "Javascript",
     },
     {
       repo: "https://github.com/KClarkDev/puppy-love",
       url: "https://kclarkdev.github.io/puppy-love/",
-      imgUrl: "./src/assets/puppy-love-husky.jpg",
+      imgUrl: puppyLove,
       title: "Puppy Love",
       details: "CSS Animations",
     },
     {
       repo: "https://github.com/KClarkDev/pomodoro-timer",
-      imgUrl: "src/assets/pomodoro.jfif",
+      imgUrl: pomodoro,
       title: "Pomodoro Timer",
       details: "Python",
     },
     {
       repo: "https://github.com/KClarkDev/weather-underground-web-scraper",
-      imgUrl: "src/assets/wu.png",
+      imgUrl: weatherUnderground,
       title: "Web Scraping",
       details: "Python",
     },
     {
       repo: "https://github.com/KClarkDev/work-day-scheduler",
       url: "https://kclarkdev.github.io/work-day-scheduler/",
-      imgUrl: "src/assets/planner.jpeg",
+      imgUrl: planner,
       title: "Workday Scheduler",
       details: "Javascript",
     },
